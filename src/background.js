ac4d4b0da11d9cb6acf005e92b6cb645e944e600
JixunMoe <Jixun.Moe@Gmail.Com>
2015-12-06 13:13:06 +0000

    Update to 0.2 (Netcom CDN)

--- src/background.js
+++ src/background.js
@@ -1,5 +1,5 @@
 chrome.webRequest.onBeforeRequest.addListener(function (details) {
-  return { redirectUrl: details.url.replace('http://m', 'http://p') };
+  return { redirectUrl: details.url.replace('http://', 'http://203.130.59.9/') };
 }, {
   urls: ["http://*.music.126.net/*"]
 }, ["blocking"]);
